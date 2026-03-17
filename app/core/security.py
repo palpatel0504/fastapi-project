c25e983f79c52a652d7097800c654cf2e7b2691e
palpatel0504 <127982372+palpatel0504@users.noreply.github.com>
2026-03-17 17:07:51 +0530

    added app configurations

---
+++ app/core/security.py
@@ -0,0 +1,15 @@
+from datetime import datetime , timezone,timedelta
+from jose import jwt,JWTERROR
+from app.core.config import settings
+
+
+
+def create_token(data:dict,expire_minutes=30):
+    to_encode=data.copy()
+    expire = datetime.now(timezone.utc)+ timedelta(minutes=expire_minutes)
+    to_encode.update({'exp':expire})
+    return jwt.encode(
+        to_encode,
+        settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM
+    )
